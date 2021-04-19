93ce26c9f987df7dbd1d7254917ff024f101a44a
Hariharan-SV <harikumar20112000@gmail.com>
2021-04-19 10:43:54 +0530

    Add react-native-icon for home screen

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
-import { Text, Icon, Switch } from 'galio-framework'
-import { View, Button, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Text,Switch } from 'galio-framework'
+import { View, StyleSheet } from 'react-native';
 
 const App = () => {
   const [status, setStatus] = useState(false);
 
+  const getIcon = () => {
+    if(status == false)
+        return "lightbulb-off"
+    return "lightbulb-on"
+  };
+
   return (
     <View style={styles.container}>
         <Text h1 style={{"color":"#fff","alignItems":"flex-start"}}>Smart Home</Text>
-        <Icon name="quote" family="Galio" color={'#FFF'} size={50} />
+        <Icon name={getIcon()} color={'#FFF'} size={50} />
         <View style={{"alignItems":"center"}}>
             <Text h5 style={{"color":"#fafafa","alignItems":"flex-start"}}>Toggle Device status</Text>
             <Switch
                 value={status}
                 onChange={() => setStatus(!status)}
-                trackColor={{true: 'violet', false: 'white'}}
+                trackColor={{true: 'orange', false: 'grey'}}
+                thumbColor="#FFF"
             />
         </View>
     </View>
